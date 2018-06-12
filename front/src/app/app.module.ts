b5a2174e8361bfcc5b4eaf7afda4ccf040719f93
AhmBen <ahmed.benrouag.pf1@gmail.com>
2018-06-12 16:53:49 +0200

    Ajout composant Aliment

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { AlimentsComponent } from './aliments/aliments.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatGridListModule, MatCardModule } from '@angular/material';
+import {FlexLayoutModule, BREAKPOINT} from '@angular/flex-layout';
+import {FormsModule } from '@angular/forms';
+import { AlimentComponent } from './aliment/aliment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    AlimentsComponent,
+    AlimentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
