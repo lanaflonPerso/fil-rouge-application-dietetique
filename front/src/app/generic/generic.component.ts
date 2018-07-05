87309c2623e84a8f8b5ec71d6168685e09e87ae2
Turbo Xav <xavier.tagliarino@gmail.com>
2018-07-05 10:24:43 +0200

    ajout jquery

--- front/src/app/generic/generic.component.ts
+++ front/src/app/generic/generic.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Config } from '../models/datas/generic-datas';
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4';
 
 @Component({
   selector: 'app-generic',
@@ -26,10 +29,31 @@ export class GenericComponent implements OnInit {
 
   public constructor() {
     this.config = new Config();
+    $(document).ready( function() {
+      const table: any = $('.table');
+      this.dataTable = table.DataTable();
+       /*const table2: any = $('#example');
+      table2.DataTable( {
+        'language': {
+            'lengthMenu': 'Nombre de repas par page _MENU_',
+            'zeroRecords': 'Aucun Résultat',
+            'info': 'Page _PAGE_ sur _PAGES_',
+            'infoEmpty': 'Aucun Résultat',
+            'infoFiltered': '(filtered from _MAX_ total records)',
+            'search':'Recherche:',
+            'oPaginate': { 'sFirst':'Début','sLast':'Fin','sNext':'Suivant','sPrevious':'Précédent'}
+
+            // remplacer tout ce qui est au dessus par ce fichier (Francais)
+            //'url': '//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json'
+        }
+    } );*/
+
+
+      }
+    );
   }
 
   ngOnInit() {
-
   }
 
 }
